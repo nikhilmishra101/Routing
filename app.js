a0ba2582ca667de84fc2053958939a519fe4436b
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-29 10:58:23 +0530

    Successfully Completed the project

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", {
 
 const app = express();
 
-bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -21,18 +21,95 @@ const articleSchema = {
   content: String,
 };
 
-app.get("/articles", function (req, res) {
-  Article.find({}, function (err, foundArticles) {
-    if (!err) {
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
-
 const Article = mongoose.model("Article", articleSchema);
+////////////////////// Request Targeting All the articles/////////////
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Successfully added a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
 
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err) {
+      if (!err) {
+        res.send("Successfully deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+/////////////////////////////Request Targeting Specific Article////////////
+
+app
+  .route("/articles/:articleTitle")
+  .get(function (req, res) {
+    Article.findOne({ title: req.params.articleTitle }, function (
+      err,
+      foundArticle
+    ) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("<h1>No Articles matching that title was found</h1>");
+      }
+    });
+  })
+  .put(function (req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated article");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  .patch(function (req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function (err) {
+        if (!err) {
+          res.send("Successfully updated Article");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  .delete(function (req, res) {
+    Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+      if (!err) {
+        res.send("Successfully Deleted the Article");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
